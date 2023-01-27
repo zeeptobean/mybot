95e4ad9839a1373e047e8a5245656ee5403ad46c
ztb <zeeptobean@gmail.com>
2023-01-27 09:22:23 +0700

    sampletest: send big files

--- MyBot.cpp
+++ MyBot.cpp
@@ -1,8 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS     //fuck msvc
 
+#pragma warning(push, 0)
 #include "MyBot.h"
 #include <dpp/dpp.h>
 #include <thread>
+#pragma warning(pop)
 
 
 /* Be sure to place your token in the line below.
@@ -22,10 +24,27 @@ int main()
     /* Output simple log messages to stdout */
     bot.on_log(dpp::utility::cout_logger());
 
+	dpp::message newmsg(&bot);
+    newmsg.set_guild_id(664744934003310592);
+    newmsg.set_channel_id(1056052934921699388);
+
+	std::ifstream infile("dump-msvc.txt");
+	infile.seekg(0, infile.end);
+	int len = infile.tellg();
+	infile.seekg(0, infile.beg);
+	char *buf = new char[len+1];
+	infile.read(buf, len);
+	std::string str = std::string(buf);
+	infile.close();
+	delete[] buf; 
+
+	newmsg.add_file("file1.txt", str);
+	newmsg.add_file("file2.txt", str);
+
     /* Handle slash command */
-    bot.on_slashcommand([](const dpp::slashcommand_t& event) {
+    bot.on_slashcommand([&newmsg](const dpp::slashcommand_t& event) {
          if (event.command.get_command_name() == "ping") {
-            event.reply("Pong!");
+            event.reply(newmsg);
         }
     });
 
@@ -37,29 +56,8 @@ int main()
         }
     });
 
-    dpp::message newmsg(&bot);
-    newmsg.set_guild_id(664744934003310592);
-    newmsg.set_channel_id(1056052934921699388);
-
-    std::thread t1([&bot, &newmsg]() {
-        char buffer[1001];
-        while (true) {
-            snprintf(buffer, 1000, "Hello world! Running on %s. Current time is %s",
-                dpp::utility::version().c_str(),
-                dpp::utility::current_date_time().c_str());
-            newmsg.set_content(std::string(buffer));
-            bot.message_create(newmsg, [](const dpp::confirmation_callback_t& tres) {
-                assert(!tres.is_error());
-                });
-            std::this_thread::sleep_for(std::chrono::seconds(5));
-        }
-    });
-    
-
     /* Start the bot */
     bot.start(false);
-
-    t1.join();
 
     return 0;
 }
